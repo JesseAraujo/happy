f008bf8a9a6b2812f06ce64644803fc6ec4ac1fb
JesseAraujo <jesse.araujo4@fatec.sp.gov.br>
2021-04-05 19:37:10 -0300

    criação de tabelas e início upload imagens

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
-import express, { request, response } from 'express'
+import express, { request, response } from "express";
 
-import './database/connection'
+import "./database/connection";
+import routes from "./routes";
 
-const app = express()
+const app = express();
 
-app.get("/users", (request, response) => { 
-    return response.json({ message : 'Hello World' })
-})
-app.listen(3333)
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
